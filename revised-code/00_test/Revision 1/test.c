7e0733ad21aaa151483b31c08bcc74b3dba0fd1b
George Pimpleton <george_pimpleton@hotmail.com>
2023-10-24 10:49:40 -0700

    00_test

--- revised-code/00_test/Revision 1/test.c
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-#include <tchar.h>
-
-int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrev, PTSTR pszCmdLine, int iCmdShow)
-{
-	MessageBox(NULL, TEXT("Narf!"), TEXT("Pinky says..."), MB_OK | MB_ICONEXCLAMATION);
-	return 0;
-}
