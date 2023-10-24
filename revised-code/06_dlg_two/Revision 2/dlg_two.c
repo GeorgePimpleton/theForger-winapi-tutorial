2a5fd88c7cfde143bfdbf4470b7f900b2d61ec64
George Pimpleton <george_pimpleton@hotmail.com>
2023-10-24 10:06:15 -0700

    06_dlg_two

--- revised-code/06_dlg_two/Revision 2/dlg_two.c
+++ revised-code/06_dlg_two/Revision 2/dlg_two.c
@@ -2,24 +2,24 @@
 
 #include "resource.h"
 
-const WCHAR g_szClassName[ ] = L"myWindowClass";
+PCWSTR g_className = L"myWindowClass";
 
-HWND g_hToolbar = NULL;
+HWND   g_toolbar   = NULL;
 
-BOOL CALLBACK ToolDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
+BOOL CALLBACK ToolDlgProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-   switch (Message)
+   switch (msg)
    {
    case WM_COMMAND:
       switch (LOWORD(wParam))
       {
       case IDC_PRESS:
-         MessageBoxW(hwnd, L"Hi!", L"This is a message",
+         MessageBoxW(wnd, L"Hi!", L"This is a message",
                      MB_OK | MB_ICONEXCLAMATION);
          break;
 
       case IDC_OTHER:
-         MessageBoxW(hwnd, L"Bye!", L"This is also a message",
+         MessageBoxW(wnd, L"Bye!", L"This is also a message",
                      MB_OK | MB_ICONEXCLAMATION);
          break;
       }
@@ -31,20 +31,21 @@ BOOL CALLBACK ToolDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
    return TRUE;
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-   switch (Message)
+   switch (msg)
    {
    case WM_CREATE:
-      g_hToolbar = CreateDialogW(GetModuleHandleW(NULL), MAKEINTRESOURCEW(IDD_TOOLBAR),
-                                 hwnd, (DLGPROC) ToolDlgProc);
-      if (g_hToolbar != NULL)
+      g_toolbar = CreateDialogW(GetModuleHandleW(NULL), MAKEINTRESOURCEW(IDD_TOOLBAR),
+                                 wnd, (DLGPROC) ToolDlgProc);
+
+      if (g_toolbar != NULL)
       {
-         ShowWindow(g_hToolbar, SW_SHOW);
+         ShowWindow(g_toolbar, SW_SHOW);
       }
       else
       {
-         MessageBoxW(hwnd, L"CreateDialog returned NULL", L"Warning!",
+         MessageBoxW(wnd, L"CreateDialog returned NULL", L"Warning!",
                     MB_OK | MB_ICONINFORMATION);
       }
       break;
@@ -53,52 +54,58 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
       switch (LOWORD(wParam))
       {
       case ID_FILE_EXIT:
-         PostMessageW(hwnd, WM_CLOSE, 0, 0);
+         PostMessageW(wnd, WM_CLOSE, 0, 0);
          break;
 
       case ID_DIALOG_SHOW:
-         ShowWindow(g_hToolbar, SW_SHOW);
+         ShowWindow(g_toolbar, SW_SHOW);
          break;
 
       case ID_DIALOG_HIDE:
-         ShowWindow(g_hToolbar, SW_HIDE);
+         ShowWindow(g_toolbar, SW_HIDE);
          break;
       }
       break;
 
    case WM_CLOSE:
-      DestroyWindow(hwnd);
+      DestroyWindow(wnd);
       break;
 
    case WM_DESTROY:
-      DestroyWindow(g_hToolbar);
+      DestroyWindow(g_toolbar);
       PostQuitMessage(0);
       break;
+
    default:
-      return DefWindowProc(hwnd, Message, wParam, lParam);
+      return DefWindowProcW(wnd, msg, wParam, lParam);
    }
    return 0;
 }
 
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
-                    _In_ PWSTR lpCmdLine, _In_ int nCmdShow)
+int WINAPI wWinMain(_In_     HINSTANCE inst,
+                    _In_opt_ HINSTANCE prevInst,
+                    _In_     PWSTR     cmdLine,
+                    _In_     int       cmdShow)
 {
-   WNDCLASSEXW wc;
-   HWND hwnd;
-   MSG Msg;
+   UNREFERENCED_PARAMETER(prevInst);
+   UNREFERENCED_PARAMETER(cmdLine);
+
+   WNDCLASSEXW wc = { 0 };
+   HWND        wnd;
+   MSG         msg;
 
    wc.cbSize        = sizeof(WNDCLASSEXW);
    wc.style         = 0;
    wc.lpfnWndProc   = WndProc;
    wc.cbClsExtra    = 0;
    wc.cbWndExtra    = 0;
-   wc.hInstance     = hInstance;
+   wc.hInstance     = inst;
    wc.hIcon         = (HICON)   LoadImageW(NULL, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
    wc.hIconSm       = (HICON)   LoadImageW(NULL, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
    wc.hCursor       = (HCURSOR) LoadImageW(NULL, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED);
    wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
    wc.lpszMenuName  = MAKEINTRESOURCEW(IDR_MYMENU);
-   wc.lpszClassName = g_szClassName;
+   wc.lpszClassName = g_className;
 
    if (!RegisterClassExW(&wc))
    {
@@ -107,30 +114,29 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
       return 0;
    }
 
-   hwnd = CreateWindowExW(WS_EX_CLIENTEDGE,
-                          g_szClassName,
-                          L"The title of my window",
+   wnd = CreateWindowExW(WS_EX_CLIENTEDGE,
+                          g_className, L"The title of my window",
                           WS_OVERLAPPEDWINDOW,
                           CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
-                          NULL, NULL, hInstance, NULL);
+                          NULL, NULL, inst, NULL);
 
-   if (hwnd == NULL)
+   if (wnd == NULL)
    {
       MessageBoxW(NULL, L"Window Creation Failed!", L"Error!",
                   MB_ICONEXCLAMATION | MB_OK);
       return 0;
    }
 
-   ShowWindow(hwnd, nCmdShow);
-   UpdateWindow(hwnd);
+   ShowWindow(wnd, cmdShow);
+   UpdateWindow(wnd);
 
-   while (GetMessageW(&Msg, NULL, 0, 0) > 0)
+   while (GetMessageW(&msg, NULL, 0, 0) > 0)
    {
-      if (!IsDialogMessageW(g_hToolbar, &Msg))
+      if (!IsDialogMessageW(g_toolbar, &msg))
       {
-         TranslateMessage(&Msg);
-         DispatchMessageW(&Msg);
+         TranslateMessage(&msg);
+         DispatchMessageW(&msg);
       }
    }
-   return (int) Msg.wParam;
+   return (int) msg.wParam;
 }
--- revised-code/06_dlg_two/Revision 2/resource.h
+++ revised-code/06_dlg_two/Revision 2/resource.h
@@ -1,7 +1,5 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by dlg_two.rc
-//
+#pragma once
+
 #define IDR_MYMENU                      101
 #define IDD_TOOLBAR                     101
 #define IDC_PRESS                       1000
@@ -9,14 +7,3 @@
 #define ID_FILE_EXIT                    40001
 #define ID_DIALOG_SHOW                  40002
 #define ID_DIALOG_HIDE                  40003
-
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40004
-#define _APS_NEXT_CONTROL_VALUE         1002
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
