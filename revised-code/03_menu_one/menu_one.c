cad054b4ad2259e3863a4651fe7b16a5e74c1b09
George Pimpleton <george_pimpleton@hotmail.com>
2023-11-09 08:07:31 -0800

    03_menu_one

--- revised-code/03_menu_one/menu_one.c
+++ revised-code/03_menu_one/menu_one.c
@@ -1,85 +1,88 @@
 #include <windows.h>
+#include "resource.h"
 
-#include "resource.h" 
+PCWSTR g_className = L"myWindowClass";
 
-const char g_szClassName[] = "myWindowClass";
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	switch(Message)
-	{
-		case WM_COMMAND:
-			switch(LOWORD(wParam))
-			{
-				case ID_FILE_EXIT:
-					PostMessage(hwnd, WM_CLOSE, 0, 0);
-				break;
-				case ID_STUFF_GO:
-					MessageBox(hwnd, "You clicked Go!", "Woo!", MB_OK);
-				break;
-			}
-		break;
-		case WM_CLOSE:
-			DestroyWindow(hwnd);
-		break;
-		case WM_DESTROY:
-			PostQuitMessage(0);
-		break;
-		default:
-			return DefWindowProc(hwnd, Message, wParam, lParam);
-	}
-	return 0;
+   switch ( msg )
+   {
+   case WM_COMMAND:
+      switch ( LOWORD(wParam) )
+      {
+
+      case ID_FILE_EXIT:
+         PostMessageW(wnd, WM_CLOSE, 0, 0);
+         break;
+
+      case ID_STUFF_GO:
+         MessageBoxW(wnd, L"You clicked Go!", L"Woo!", MB_OK);
+         break;
+      }
+      break;
+
+   case WM_CLOSE:
+      DestroyWindow(wnd);
+      break;
+
+   case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
+
+   default:
+      return DefWindowProcW(wnd, msg, wParam, lParam);
+   }
+   return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine, int nCmdShow)
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInst,
+                    _In_ PWSTR cmdLine, _In_ int cmdShow)
 {
-	WNDCLASSEX wc;
-	HWND hwnd;
-	MSG Msg;
+   WNDCLASSEXW wc  = { 0 };
+   HWND        wnd;
+   MSG         msg;
 
-	wc.cbSize		 = sizeof(WNDCLASSEX);
-	wc.style		 = 0;
-	wc.lpfnWndProc	 = WndProc;
-	wc.cbClsExtra	 = 0;
-	wc.cbWndExtra	 = 0;
-	wc.hInstance	 = hInstance;
-	wc.hIcon		 = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
-	wc.hCursor		 = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-	wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MYMENU);
-	wc.lpszClassName = g_szClassName;
-	wc.hIconSm		 = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, 16, 16, 0);
+   wc.cbSize        = sizeof(WNDCLASSEXW);
+   wc.style         = 0;
+   wc.lpfnWndProc   = WndProc;
+   wc.cbClsExtra    = 0;
+   wc.cbWndExtra    = 0;
+   wc.hInstance     = hInstance;
+   wc.hIcon         = (HICON) LoadImageW(GetModuleHandleW(NULL), MAKEINTRESOURCEW(IDI_MYICON), IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
+   wc.hCursor       = (HCURSOR) LoadImageW(NULL, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED);
+   wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+   wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MYMENU);
+   wc.lpszClassName = g_className;
+   wc.hIconSm       = (HICON) LoadImageW(GetModuleHandleW(NULL), MAKEINTRESOURCEW(IDI_MYICON), IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
 
-	if(!RegisterClassEx(&wc))
-	{
-		MessageBox(NULL, "Window Registration Failed!", "Error!",
-			MB_ICONEXCLAMATION | MB_OK);
-		return 0;
-	}
+   if ( !RegisterClassExW(&wc) )
+   {
+      MessageBoxW(NULL, L"Window Registration Failed!", L"Error!",
+                  MB_ICONEXCLAMATION | MB_OK);
+      return 0;
+   }
 
-	hwnd = CreateWindowEx(
-		WS_EX_CLIENTEDGE,
-		g_szClassName,
-		"A Menu",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
-		NULL, NULL, hInstance, NULL);
+   wnd = CreateWindowExW(WS_EX_CLIENTEDGE,
+                         g_className,
+                         L"A Menu",
+                         WS_OVERLAPPEDWINDOW,
+                         CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+                         NULL, NULL, hInstance, NULL);
 
-	if(hwnd == NULL)
-	{
-		MessageBox(NULL, "Window Creation Failed!", "Error!",
-			MB_ICONEXCLAMATION | MB_OK);
-		return 0;
-	}
+   if ( wnd == NULL )
+   {
+      MessageBoxW(NULL, L"Window Creation Failed!", L"Error!",
+                  MB_ICONEXCLAMATION | MB_OK);
+      return 0;
+   }
 
-	ShowWindow(hwnd, nCmdShow);
-	UpdateWindow(hwnd);
+   ShowWindow(wnd, cmdShow);
+   UpdateWindow(wnd);
 
-	while(GetMessage(&Msg, NULL, 0, 0) > 0)
-	{
-		TranslateMessage(&Msg);
-		DispatchMessage(&Msg);
-	}
-	return Msg.wParam;
+   while ( GetMessageW(&msg, NULL, 0, 0) > 0 )
+   {
+      TranslateMessage(&msg);
+      DispatchMessageW(&msg);
+   }
+   return (int) msg.wParam;
 }
