75cb89aa6e336b6b8acd06b3816431b13ec83972
George Pimpleton <george_pimpleton@hotmail.com>
2023-11-14 06:51:51 -0800

    11_app_three

--- revised-code/11_app_three/app_three.c
+++ revised-code/11_app_three/app_three.c
@@ -4,174 +4,189 @@
 
 #pragma comment (lib, "comctl32.lib")
 
-const WCHAR g_szClassName[ ] = L"myWindowClass";
-
 #define IDC_MAIN_EDIT	101
 #define IDC_MAIN_TOOL	102
 #define IDC_MAIN_STATUS	103
 
-BOOL LoadTextFileToEdit(HWND hEdit, PCWSTR pszFileName)
+BOOL LoadTextFileToEdit(HWND edit, PCWSTR fileName)
 {
-   HANDLE hFile;
-   BOOL   bSuccess = FALSE;
+   HANDLE file;
+   BOOL   success = FALSE;
 
-   hFile = CreateFileW(pszFileName, GENERIC_READ, FILE_SHARE_READ, NULL,
-                       OPEN_EXISTING, 0, NULL);
-   if (hFile != INVALID_HANDLE_VALUE)
+   file = CreateFileW(fileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+
+   if ( file != INVALID_HANDLE_VALUE )
    {
-      DWORD dwFileSize;
+      DWORD fileSize;
 
-      dwFileSize = GetFileSize(hFile, NULL);
-      if (dwFileSize != 0xFFFFFFFF)
+      fileSize = GetFileSize(file, NULL);
+
+      if ( fileSize != 0xFFFFFFFF )
       {
-         PSTR pszFileText;
+         PSTR fileText;
 
-         pszFileText = (PSTR) GlobalAlloc(GPTR, dwFileSize + 1);
-         if (pszFileText != NULL)
+         fileText = (PSTR) GlobalAlloc(GPTR, fileSize + 1);
+
+         if ( fileText != NULL )
          {
-            DWORD dwRead;
+            DWORD read;
 
-            if (ReadFile(hFile, pszFileText, dwFileSize, &dwRead, NULL))
+            if ( ReadFile(file, fileText, fileSize, &read, NULL) )
             {
-               pszFileText[dwFileSize] = 0; // Add null terminator
-               if (SetWindowTextA(hEdit, pszFileText))
-                  bSuccess = TRUE; // It worked!
+               fileText[fileSize] = 0; // Add null terminator
+
+               if ( SetWindowTextA(edit, fileText) )
+               {
+                  success = TRUE; // It worked!
+               }
             }
-            GlobalFree(pszFileText);
+            GlobalFree(fileText);
          }
       }
-      CloseHandle(hFile);
+      CloseHandle(file);
    }
-   return bSuccess;
+   return success;
 }
 
-BOOL SaveTextFileFromEdit(HWND hEdit, PCWSTR pszFileName)
+BOOL SaveTextFileFromEdit(HWND edit, PCWSTR fileName)
 {
-   HANDLE hFile;
-   BOOL   bSuccess = FALSE;
+   HANDLE file;
+   BOOL   success = FALSE;
 
-   hFile = CreateFileW(pszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-   if (hFile != INVALID_HANDLE_VALUE)
+   file = CreateFileW(fileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+   if ( file != INVALID_HANDLE_VALUE )
    {
-      DWORD dwTextLength;
+      DWORD textLength;
 
-      dwTextLength = GetWindowTextLengthW(hEdit);
+      textLength = GetWindowTextLengthW(edit);
+
       // No need to bother if there's no text.
-      if (dwTextLength > 0)
+      if ( textLength > 0 )
       {
-         PSTR  pszText;
-         DWORD dwBufferSize = dwTextLength + 1;
+         PSTR  text;
+         DWORD bufferSize = textLength + 1;
 
-         pszText = (PSTR) GlobalAlloc(GPTR, dwBufferSize);
-         if (pszText != NULL)
+         text = (PSTR) GlobalAlloc(GPTR, bufferSize);
+
+         if ( text != NULL )
          {
-            if (GetWindowTextA(hEdit, pszText, dwBufferSize))
+            if ( GetWindowTextA(edit, text, bufferSize) )
             {
-               DWORD dwWritten;
+               DWORD written;
 
-               if (WriteFile(hFile, pszText, dwTextLength, &dwWritten, NULL))
-                  bSuccess = TRUE;
+               if ( WriteFile(file, text, textLength, &written, NULL) )
+               {
+                  success = TRUE;
+               }
             }
-            GlobalFree(pszText);
+            GlobalFree(text);
          }
       }
-      CloseHandle(hFile);
+      CloseHandle(file);
    }
-   return bSuccess;
+   return success;
 }
 
-void DoFileOpen(HWND hwnd)
+void DoFileOpen(HWND wnd)
 {
    OPENFILENAMEW ofn;
-   WCHAR         szFileName[MAX_PATH] = L"";
+   WCHAR         fileName[MAX_PATH] = L"";
 
    ZeroMemory(&ofn, sizeof(ofn));
 
    ofn.lStructSize = sizeof(ofn);
-   ofn.hwndOwner   = hwnd;
+   ofn.hwndOwner   = wnd;
    ofn.lpstrFilter = L"Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
-   ofn.lpstrFile   = szFileName;
+   ofn.lpstrFile   = fileName;
    ofn.nMaxFile    = MAX_PATH;
    ofn.Flags       = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
    ofn.lpstrDefExt = L"txt";
 
-   if (GetOpenFileNameW(&ofn))
+   if ( GetOpenFileNameW(&ofn) )
    {
-      HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
-      if (LoadTextFileToEdit(hEdit, szFileName))
+      HWND edit = GetDlgItem(wnd, IDC_MAIN_EDIT);
+
+      if ( LoadTextFileToEdit(edit, fileName) )
       {
-         SendDlgItemMessageW(hwnd, IDC_MAIN_STATUS, SB_SETTEXT, 0, (LPARAM) L"Opened...");
-         SendDlgItemMessageW(hwnd, IDC_MAIN_STATUS, SB_SETTEXT, 1, (LPARAM) szFileName);
+         SendDlgItemMessageW(wnd, IDC_MAIN_STATUS, SB_SETTEXT, 0, (LPARAM) L"Opened...");
+         SendDlgItemMessageW(wnd, IDC_MAIN_STATUS, SB_SETTEXT, 1, (LPARAM) fileName);
       }
    }
 }
 
-void DoFileSave(HWND hwnd)
+void DoFileSave(HWND wnd)
 {
    OPENFILENAMEW ofn;
-   WCHAR         szFileName[MAX_PATH] = L"";
+   WCHAR         fileName[MAX_PATH] = L"";
 
    ZeroMemory(&ofn, sizeof(ofn));
 
    ofn.lStructSize = sizeof(ofn);
-   ofn.hwndOwner   = hwnd;
+   ofn.hwndOwner   = wnd;
    ofn.lpstrFilter = L"Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
-   ofn.lpstrFile   = szFileName;
+   ofn.lpstrFile   = fileName;
    ofn.nMaxFile    = MAX_PATH;
    ofn.lpstrDefExt = L"txt";
    ofn.Flags       = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
 
-   if (GetSaveFileNameW(&ofn))
+   if ( GetSaveFileNameW(&ofn) )
    {
-      HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
-      if (SaveTextFileFromEdit(hEdit, szFileName))
+      HWND edit = GetDlgItem(wnd, IDC_MAIN_EDIT);
+
+      if ( SaveTextFileFromEdit(edit, fileName) )
       {
-         SendDlgItemMessageW(hwnd, IDC_MAIN_STATUS, SB_SETTEXT, 0, (LPARAM) L"Saved...");
-         SendDlgItemMessageA(hwnd, IDC_MAIN_STATUS, SB_SETTEXT, 1, (LPARAM) szFileName);
+         SendDlgItemMessageW(wnd, IDC_MAIN_STATUS, SB_SETTEXT, 0, (LPARAM) L"Saved...");
+         SendDlgItemMessageA(wnd, IDC_MAIN_STATUS, SB_SETTEXT, 1, (LPARAM) fileName);
       }
    }
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-   switch (msg)
+   switch ( msg )
    {
    case WM_CREATE:
    {
-      HFONT hfDefault;
-      HWND  hEdit;
+      HFONT fontDefault;
+      HWND  edit;
 
-      HWND        hTool;
+      HWND        tool;
       TBBUTTON    tbb[3];
       TBADDBITMAP tbab;
 
-      HWND hStatus;
+      HWND status;
       int  statwidths[ ] = { 100, -1 };
 
       // Create Edit Control
-      hEdit = CreateWindowExW(WS_EX_CLIENTEDGE, L"EDIT", L"",
+      edit = CreateWindowExW(WS_EX_CLIENTEDGE, L"EDIT", L"",
                               WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_HSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
-                              0, 0, 100, 100, hwnd, (HMENU) IDC_MAIN_EDIT, GetModuleHandleW(NULL), NULL);
-      if (hEdit == NULL)
-         MessageBoxW(hwnd, L"Could not create edit box.", L"Error", MB_OK | MB_ICONERROR);
+                              0, 0, 100, 100, wnd, (HMENU) IDC_MAIN_EDIT, GetModuleHandleW(NULL), NULL);
+      if ( edit == NULL )
+      {
+         MessageBoxW(wnd, L"Could not create edit box.", L"Error", MB_OK | MB_ICONERROR);
+      }
 
-      hfDefault = (HFONT) GetStockObject(DEFAULT_GUI_FONT);
+      fontDefault = (HFONT) GetStockObject(DEFAULT_GUI_FONT);
    #pragma warning (disable : 6387)
-      SendMessageW(hEdit, WM_SETFONT, (WPARAM) hfDefault, MAKELPARAM(FALSE, 0));
+      SendMessageW(edit, WM_SETFONT, (WPARAM) fontDefault, MAKELPARAM(FALSE, 0));
 
       // Create Toolbar
-      hTool = CreateWindowExW(0, TOOLBARCLASSNAMEW, NULL, WS_CHILD | WS_VISIBLE, 0, 0, 0, 0,
-                              hwnd, (HMENU) IDC_MAIN_TOOL, GetModuleHandleW(NULL), NULL);
-      if (hTool == NULL)
-         MessageBoxW(hwnd, L"Could not create tool bar.", L"Error", MB_OK | MB_ICONERROR);
+      tool = CreateWindowExW(0, TOOLBARCLASSNAMEW, NULL, WS_CHILD | WS_VISIBLE, 0, 0, 0, 0,
+                              wnd, (HMENU) IDC_MAIN_TOOL, GetModuleHandleW(NULL), NULL);
+
+      if ( tool == NULL )
+      {
+         MessageBoxW(wnd, L"Could not create tool bar.", L"Error", MB_OK | MB_ICONERROR);
+      }
 
       // Send the TB_BUTTONSTRUCTSIZE message, which is required for
       // backward compatibility.
-      SendMessageW(hTool, TB_BUTTONSTRUCTSIZE, (WPARAM) sizeof(TBBUTTON), 0);
+      SendMessageW(tool, TB_BUTTONSTRUCTSIZE, (WPARAM) sizeof(TBBUTTON), 0);
 
-      tbab.hInst = HINST_COMMCTRL;
-      tbab.nID   = IDB_STD_SMALL_COLOR;
-      SendMessageW(hTool, TB_ADDBITMAP, 0, (LPARAM) &tbab);
+      tbab.hInst         = HINST_COMMCTRL;
+      tbab.nID           = IDB_STD_SMALL_COLOR;
+      SendMessageW(tool, TB_ADDBITMAP, 0, (LPARAM) &tbab);
 
       ZeroMemory(tbb, sizeof(tbb));
       tbb[0].iBitmap   = STD_FILENEW;
@@ -189,58 +204,58 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
       tbb[2].fsStyle   = TBSTYLE_BUTTON;
       tbb[2].idCommand = ID_FILE_SAVEAS;
 
-      SendMessageW(hTool, TB_ADDBUTTONS, sizeof(tbb) / sizeof(TBBUTTON), (LPARAM) &tbb);
+      SendMessageW(tool, TB_ADDBUTTONS, sizeof(tbb) / sizeof(TBBUTTON), (LPARAM) &tbb);
 
       // Create Status bar
-      hStatus = CreateWindowExW(0, STATUSCLASSNAMEW, NULL,
+      status = CreateWindowExW(0, STATUSCLASSNAMEW, NULL,
                                 WS_CHILD | WS_VISIBLE | SBARS_SIZEGRIP, 0, 0, 0, 0,
-                                hwnd, (HMENU) IDC_MAIN_STATUS, GetModuleHandleW(NULL), NULL);
+                                wnd, (HMENU) IDC_MAIN_STATUS, GetModuleHandleW(NULL), NULL);
 
-      SendMessageW(hStatus, SB_SETPARTS, sizeof(statwidths) / sizeof(int), (LPARAM) statwidths);
-      SendMessageW(hStatus, SB_SETTEXT, 0, (LPARAM) L"Hi there :)");
+      SendMessageW(status, SB_SETPARTS, sizeof(statwidths) / sizeof(int), (LPARAM) statwidths);
+      SendMessageW(status, SB_SETTEXT, 0, (LPARAM) L"Hi there :)");
    }
    break;
 
    case WM_SIZE:
    {
-      HWND hTool;
-      RECT rcTool;
-      int  iToolHeight;
+      HWND tool;
+      RECT toolRect;
+      int  toolHeight;
 
-      HWND hStatus;
-      RECT rcStatus;
-      int  iStatusHeight;
+      HWND status;
+      RECT statusRect;
+      int  statusHeight;
 
-      HWND hEdit;
-      int  iEditHeight;
-      RECT rcClient;
+      HWND edit;
+      int  editHeight;
+      RECT clientRect;
 
       // Size toolbar and get height
-      hTool = GetDlgItem(hwnd, IDC_MAIN_TOOL);
-      SendMessageW(hTool, TB_AUTOSIZE, 0, 0);
+      tool = GetDlgItem(wnd, IDC_MAIN_TOOL);
+      SendMessageW(tool, TB_AUTOSIZE, 0, 0);
 
-      GetWindowRect(hTool, &rcTool);
-      iToolHeight = rcTool.bottom - rcTool.top;
+      GetWindowRect(tool, &toolRect);
+      toolHeight = toolRect.bottom - toolRect.top;
 
       // Size status bar and get height
-      hStatus = GetDlgItem(hwnd, IDC_MAIN_STATUS);
-      SendMessageW(hStatus, WM_SIZE, 0, 0);
+      status = GetDlgItem(wnd, IDC_MAIN_STATUS);
+      SendMessageW(status, WM_SIZE, 0, 0);
 
-      GetWindowRect(hStatus, &rcStatus);
-      iStatusHeight = rcStatus.bottom - rcStatus.top;
+      GetWindowRect(status, &statusRect);
+      statusHeight = statusRect.bottom - statusRect.top;
 
       // Calculate remaining height and size edit
-      GetClientRect(hwnd, &rcClient);
+      GetClientRect(wnd, &clientRect);
 
-      iEditHeight = rcClient.bottom - iToolHeight - iStatusHeight;
+      editHeight = clientRect.bottom - toolHeight - statusHeight;
 
-      hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
-      SetWindowPos(hEdit, NULL, 0, iToolHeight, rcClient.right, iEditHeight, SWP_NOZORDER);
+      edit = GetDlgItem(wnd, IDC_MAIN_EDIT);
+      SetWindowPos(edit, NULL, 0, toolHeight, clientRect.right, editHeight, SWP_NOZORDER);
    }
    break;
 
    case WM_CLOSE:
-      DestroyWindow(hwnd);
+      DestroyWindow(wnd);
       break;
 
    case WM_DESTROY:
@@ -248,80 +263,84 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
       break;
 
    case WM_COMMAND:
-      switch (LOWORD(wParam))
+      switch ( LOWORD(wParam) )
       {
       case ID_FILE_EXIT:
-         PostMessageW(hwnd, WM_CLOSE, 0, 0);
+         PostMessageW(wnd, WM_CLOSE, 0, 0);
          break;
 
       case ID_FILE_NEW:
-         SetDlgItemTextA(hwnd, IDC_MAIN_EDIT, "");
+         SetDlgItemTextA(wnd, IDC_MAIN_EDIT, "");
          break;
 
       case ID_FILE_OPEN:
-         DoFileOpen(hwnd);
+         DoFileOpen(wnd);
          break;
 
       case ID_FILE_SAVEAS:
-         DoFileSave(hwnd);
+         DoFileSave(wnd);
          break;
       }
       break;
 
    default:
-      return DefWindowProcW(hwnd, msg, wParam, lParam);
+      return DefWindowProcW(wnd, msg, wParam, lParam);
    }
    return 0;
 }
 
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
-                    _In_ PWSTR lpCmdLine, _In_ int nCmdShow)
+int WINAPI wWinMain(_In_ HINSTANCE inst, _In_opt_ HINSTANCE prevInst,
+                    _In_ PWSTR cmdLine, _In_ int cmdShow)
 {
-   WNDCLASSEXW wc;
-   HWND        hwnd;
-   MSG         Msg;
+   UNREFERENCED_PARAMETER(prevInst);
+   UNREFERENCED_PARAMETER(cmdLine);
 
-   InitCommonControls();
+   WNDCLASSEXW wc        = { 0 };
+   HWND        wnd;
+   MSG         msg;
+   PCWSTR      className = L"myWindowClass";
+
+   InitCommonControls( );
 
    wc.cbSize        = sizeof(WNDCLASSEXW);
    wc.style         = 0;
    wc.lpfnWndProc   = WndProc;
    wc.cbClsExtra    = 0;
    wc.cbWndExtra    = 0;
-   wc.hInstance     = hInstance;
+   wc.hInstance     = inst;
    wc.hIcon         = (HICON)   LoadImageW(NULL, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
    wc.hIconSm       = (HICON)   LoadImageW(NULL, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
    wc.hCursor       = (HCURSOR) LoadImageW(NULL, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED);
-   wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+   wc.hbrBackground = (HBRUSH)  (COLOR_WINDOW + 1);
    wc.lpszMenuName  = MAKEINTRESOURCEW(IDR_MAINMENU);
-   wc.lpszClassName = g_szClassName;
+   wc.lpszClassName = className;
 
-   if (!RegisterClassExW(&wc))
+   if ( !RegisterClassExW(&wc) )
    {
       MessageBoxW(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
       return 0;
    }
 
-   hwnd = CreateWindowExW(0,
-                          g_szClassName,
-                          L"theForger's Tutorial Application",
-                          WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
-                          CW_USEDEFAULT, CW_USEDEFAULT, 480, 320,
-                          NULL, NULL, hInstance, NULL);
+   wnd = CreateWindowExW(0,
+                         className,
+                         L"theForger's Tutorial Application",
+                         WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+                         CW_USEDEFAULT, CW_USEDEFAULT, 480, 320,
+                         NULL, NULL, inst, NULL);
 
-   if (hwnd == NULL)
+   if ( wnd == NULL )
    {
       MessageBoxW(NULL, L"Window Creation Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
       return 0;
    }
 
-   ShowWindow(hwnd, nCmdShow);
-   UpdateWindow(hwnd);
+   ShowWindow(wnd, cmdShow);
+   UpdateWindow(wnd);
 
-   while (GetMessageW(&Msg, NULL, 0, 0) > 0)
+   while ( GetMessageW(&msg, NULL, 0, 0) > 0 )
    {
-      TranslateMessage(&Msg);
-      DispatchMessageW(&Msg);
+      TranslateMessage(&msg);
+      DispatchMessageW(&msg);
    }
-   return (int) Msg.wParam;
+   return (int) msg.wParam;
 }
