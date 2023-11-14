85395c35792855596e27af2e6b128931f5a37514
George Pimpleton <george_pimpleton@hotmail.com>
2023-11-13 16:05:23 -0800

    09_app_one

--- revised-code/09_app_one/app_one.c
+++ revised-code/09_app_one/app_one.c
@@ -1,44 +1,45 @@
 #include <windows.h>
 
-const WCHAR g_szClassName[ ] = L"myWindowClass";
-
 #define IDC_MAIN_EDIT	101
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-   switch (msg)
+   switch ( msg )
    {
    case WM_CREATE:
    {
-      HFONT hfDefault;
-      HWND  hEdit;
+      HFONT fontDefault;
+      HWND  edit;
 
-      hEdit = CreateWindowExW(WS_EX_CLIENTEDGE, L"EDIT", L"",
+      edit = CreateWindowExW(WS_EX_CLIENTEDGE, L"EDIT", L"",
                              WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_HSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
-                             0, 0, 100, 100, hwnd, (HMENU) IDC_MAIN_EDIT, GetModuleHandle(NULL), NULL);
-      if (hEdit == NULL)
-         MessageBoxW(hwnd, L"Could not create edit box.", L"Error", MB_OK | MB_ICONERROR);
+                             0, 0, 100, 100, wnd, (HMENU) IDC_MAIN_EDIT, GetModuleHandleW(NULL), NULL);
 
-      hfDefault = (HFONT) GetStockObject(DEFAULT_GUI_FONT);
-   #pragma warning (disable : 6387)
-      SendMessageW(hEdit, WM_SETFONT, (WPARAM) hfDefault, MAKELPARAM(FALSE, 0));
+      if ( edit == NULL )
+      {
+         MessageBoxW(wnd, L"Could not create edit box.", L"Error", MB_OK | MB_ICONERROR);
+      }
+
+      fontDefault = (HFONT) GetStockObject(DEFAULT_GUI_FONT);
+      #pragma warning (disable : 6387)
+      SendMessageW(edit, WM_SETFONT, (WPARAM) fontDefault, MAKELPARAM(FALSE, 0));
    }
    break;
 
    case WM_SIZE:
    {
-      HWND hEdit;
+      HWND edit;
       RECT rcClient;
 
-      GetClientRect(hwnd, &rcClient);
+      GetClientRect(wnd, &rcClient);
 
-      hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
-      SetWindowPos(hEdit, NULL, 0, 0, rcClient.right, rcClient.bottom, SWP_NOZORDER);
+      edit = GetDlgItem(wnd, IDC_MAIN_EDIT);
+      SetWindowPos(edit, NULL, 0, 0, rcClient.right, rcClient.bottom, SWP_NOZORDER);
    }
    break;
 
    case WM_CLOSE:
-      DestroyWindow(hwnd);
+      DestroyWindow(wnd);
       break;
 
    case WM_DESTROY:
@@ -46,59 +47,63 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
       break;
 
    default:
-      return DefWindowProcW(hwnd, msg, wParam, lParam);
+      return DefWindowProcW(wnd, msg, wParam, lParam);
    }
    return 0;
 }
 
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
-                    _In_ PWSTR lpCmdLine, _In_ int nCmdShow)
+int WINAPI wWinMain(_In_ HINSTANCE inst, _In_opt_ HINSTANCE prevInst,
+                    _In_ PWSTR cmdLine, _In_ int cmdShow)
 {
-   WNDCLASSEXW wc;
-   HWND        hwnd;
-   MSG         Msg;
+   UNREFERENCED_PARAMETER(prevInst);
+   UNREFERENCED_PARAMETER(cmdLine);
+
+   WNDCLASSEXW wc        = { 0 };
+   HWND        wnd;
+   MSG         msg;
+   PCWSTR      className = L"myWindowClass";
 
    wc.cbSize        = sizeof(WNDCLASSEXW);
    wc.style         = 0;
    wc.lpfnWndProc   = WndProc;
    wc.cbClsExtra    = 0;
    wc.cbWndExtra    = 0;
-   wc.hInstance     = hInstance;
+   wc.hInstance     = inst;
    wc.hIcon         = (HICON)   LoadImageW(NULL, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
    wc.hIconSm       = (HICON)   LoadImageW(NULL, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
    wc.hCursor       = (HCURSOR) LoadImageW(NULL, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED);
-   wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+   wc.hbrBackground = (HBRUSH)  (COLOR_WINDOW + 1);
    wc.lpszMenuName  = NULL;
-   wc.lpszClassName = g_szClassName;
+   wc.lpszClassName = className;
 
-   if (!RegisterClassExW(&wc))
+   if ( !RegisterClassExW(&wc) )
    {
       MessageBoxW(NULL, L"Window Registration Failed!", L"Error!",
-                 MB_ICONEXCLAMATION | MB_OK);
+                  MB_ICONEXCLAMATION | MB_OK);
       return 0;
    }
 
-   hwnd = CreateWindowExW(0,
-                          g_szClassName,
-                          L"theForger's Tutorial Application",
-                          WS_OVERLAPPEDWINDOW,
-                          CW_USEDEFAULT, CW_USEDEFAULT, 480, 320,
-                          NULL, NULL, hInstance, NULL);
+   wnd = CreateWindowExW(0,
+                         className,
+                         L"theForger's Tutorial Application",
+                         WS_OVERLAPPEDWINDOW,
+                         CW_USEDEFAULT, CW_USEDEFAULT, 480, 320,
+                         NULL, NULL, inst, NULL);
 
-   if (hwnd == NULL)
+   if ( wnd == NULL )
    {
       MessageBoxW(NULL, L"Window Creation Failed!", L"Error!",
-                 MB_ICONEXCLAMATION | MB_OK);
+                  MB_ICONEXCLAMATION | MB_OK);
       return 0;
    }
 
-   ShowWindow(hwnd, nCmdShow);
-   UpdateWindow(hwnd);
+   ShowWindow(wnd, cmdShow);
+   UpdateWindow(wnd);
 
-   while (GetMessageW(&Msg, NULL, 0, 0) > 0)
+   while ( GetMessageW(&msg, NULL, 0, 0) > 0 )
    {
-      TranslateMessage(&Msg);
-      DispatchMessageW(&Msg);
+      TranslateMessage(&msg);
+      DispatchMessageW(&msg);
    }
-   return (int) Msg.wParam;
+   return (int) msg.wParam;
 }
