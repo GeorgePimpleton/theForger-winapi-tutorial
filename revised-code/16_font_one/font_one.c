8812d8293d4525784e376bc87d38a0d9689ba029
George Pimpleton <george_pimpleton@hotmail.com>
2023-11-14 10:06:07 -0800

    16_font_one

--- revised-code/16_font_one/font_one.c
+++ revised-code/16_font_one/font_one.c
@@ -2,49 +2,48 @@
 
 #include "resource.h"
 
-const WCHAR g_szClassName[] = L"myWindowClass";
-
-HFONT    g_hfFont        = NULL;
-BOOL     g_bOpaque       = TRUE;
+HFONT    g_font          = NULL;
+BOOL     g_opaque        = TRUE;
 COLORREF g_rgbText       = RGB(0, 0, 0);
 COLORREF g_rgbBackground = RGB(55, 55, 55);
 
-COLORREF g_rgbCustom[ 16 ] = { 0 };
+COLORREF g_rgbCustom[16] = { 0 };
 
-void DoSelectFont(HWND hwnd)
+void DoSelectFont(HWND wnd)
 {
    CHOOSEFONTW cf = { sizeof(CHOOSEFONTW) };
    LOGFONTW    lf;
 
-   GetObjectW(g_hfFont, sizeof(LOGFONTW), &lf);
+   GetObjectW(g_font, sizeof(LOGFONTW), &lf);
 
    cf.Flags     = CF_EFFECTS | CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS;
-   cf.hwndOwner = hwnd;
+   cf.hwndOwner = wnd;
    cf.lpLogFont = &lf;
    cf.rgbColors = g_rgbText;
 
    if ( ChooseFontW(&cf) )
    {
-      HFONT hf = CreateFontIndirectW(&lf);
-      if ( hf )
+      HFONT font = CreateFontIndirectW(&lf);
+
+      if ( font )
       {
-         g_hfFont = hf;
+         g_font = font;
       }
       else
       {
-         MessageBoxW(hwnd, L"Font creation failed!", L"Error", MB_OK | MB_ICONEXCLAMATION);
+         MessageBoxW(wnd, L"Font creation failed!", L"Error", MB_OK | MB_ICONEXCLAMATION);
       }
 
       g_rgbText = cf.rgbColors;
    }
 }
 
-void DoSelectColour(HWND hwnd)
+void DoSelectColour(HWND wnd)
 {
    CHOOSECOLORW cc = { sizeof(CHOOSECOLORW) };
 
    cc.Flags        = CC_RGBINIT | CC_FULLOPEN | CC_ANYCOLOR;
-   cc.hwndOwner    = hwnd;
+   cc.hwndOwner    = wnd;
    cc.rgbResult    = g_rgbBackground;
    cc.lpCustColors = g_rgbCustom;
 
@@ -54,31 +53,31 @@ void DoSelectColour(HWND hwnd)
    }
 }
 
-void DrawClientSize(HDC hdc, RECT* prc, HFONT hf)
+void DrawClientSize(HDC dc, RECT* rect, HFONT font)
 {
-   WCHAR szSize[ 100 ];
-   WCHAR szTitle[] = L"These are the dimensions of your client area:";
-   HFONT hfOld     = (HFONT) SelectObject(hdc, hf);
+   WCHAR size[100];
+   WCHAR title[ ] = L"These are the dimensions of your client area:";
+   HFONT oldFont  = (HFONT) SelectObject(dc, font);
 
-   SetBkColor(hdc, g_rgbBackground);
-   SetTextColor(hdc, g_rgbText);
+   SetBkColor(dc, g_rgbBackground);
+   SetTextColor(dc, g_rgbText);
 
-   if ( g_bOpaque )
+   if ( g_opaque )
    {
-      SetBkMode(hdc, OPAQUE);
+      SetBkMode(dc, OPAQUE);
    }
    else
    {
-      SetBkMode(hdc, TRANSPARENT);
+      SetBkMode(dc, TRANSPARENT);
    }
 
-   DrawTextW(hdc, szTitle, -1, prc, DT_WORDBREAK);
+   DrawTextW(dc, title, -1, rect, DT_WORDBREAK);
 
-   wsprintfW(szSize, L"{%d, %d, %d, %d}", prc->left, prc->top, prc->right, prc->bottom);
+   wsprintfW(size, L"{%d, %d, %d, %d}", rect->left, rect->top, rect->right, rect->bottom);
 
-   DrawTextW(hdc, szSize, -1, prc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+   DrawTextW(dc, size, -1, rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
 
-   SelectObject(hdc, hfOld);
+   SelectObject(dc, oldFont);
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -86,7 +85,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
    switch ( msg )
    {
    case WM_CREATE:
-      g_hfFont = (HFONT) GetStockObject(DEFAULT_GUI_FONT);
+      g_font = (HFONT) GetStockObject(DEFAULT_GUI_FONT);
       break;
 
    case WM_CLOSE:
@@ -108,8 +107,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
          break;
 
       case ID_FORMAT_DEFAULTGUIFONT:
-         DeleteObject(g_hfFont);
-         g_hfFont = (HFONT) GetStockObject(DEFAULT_GUI_FONT);
+         DeleteObject(g_font);
+         g_font = (HFONT) GetStockObject(DEFAULT_GUI_FONT);
 
          InvalidateRect(hwnd, NULL, TRUE);
          UpdateWindow(hwnd);
@@ -117,20 +116,20 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
       case ID_FORMAT_TEST:
       {
-         HFONT hf;
-         HDC   hdc;
-         long  lfHeight;
+         HFONT font;
+         HDC   dc;
+         long  fontHeight;
 
-         hdc = GetDC(NULL);
-         lfHeight = -MulDiv(12, GetDeviceCaps(hdc, LOGPIXELSY), 72);
-         ReleaseDC(NULL, hdc);
+         dc         = GetDC(NULL);
+         fontHeight = -MulDiv(12, GetDeviceCaps(dc, LOGPIXELSY), 72);
+         ReleaseDC(NULL, dc);
 
-         hf = CreateFontW(lfHeight, 0, 0, 0, 0, TRUE, 0, 0, 0, 0, 0, 0, 0, L"Times New Roman");
+         font = CreateFontW(fontHeight, 0, 0, 0, 0, TRUE, 0, 0, 0, 0, 0, 0, 0, L"Times New Roman");
 
-         if ( hf )
+         if ( font )
          {
-            DeleteObject(g_hfFont);
-            g_hfFont = hf;
+            DeleteObject(g_font);
+            g_font = font;
          }
          else
          {
@@ -149,7 +148,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
          break;
 
       case ID_FORMAT_OPAQUE:
-         g_bOpaque = !g_bOpaque;
+         g_opaque = !g_opaque;
 
          InvalidateRect(hwnd, NULL, TRUE);
          UpdateWindow(hwnd);
@@ -158,25 +157,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
       break;
 
    case WM_INITMENUPOPUP:
-      CheckMenuItem((HMENU) wParam, ID_FORMAT_OPAQUE, MF_BYCOMMAND | (g_bOpaque ? MF_CHECKED : MF_UNCHECKED));
+      CheckMenuItem((HMENU) wParam, ID_FORMAT_OPAQUE, MF_BYCOMMAND | (g_opaque ? MF_CHECKED : MF_UNCHECKED));
       break;
 
    case WM_PAINT:
    {
-      RECT        rcClient;
+      RECT        client;
       PAINTSTRUCT ps;
-      HDC         hdc = BeginPaint(hwnd, &ps);
+      HDC         dc = BeginPaint(hwnd, &ps);
 
-      GetClientRect(hwnd, &rcClient);
+      GetClientRect(hwnd, &client);
 
-      DrawClientSize(hdc, &rcClient, g_hfFont);
+      DrawClientSize(dc, &client, g_font);
 
       EndPaint(hwnd, &ps);
    }
    break;
 
    case WM_DESTROY:
-      DeleteObject(g_hfFont);
+      DeleteObject(g_font);
 
       PostQuitMessage(0);
       break;
@@ -187,25 +186,29 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
    return 0;
 }
 
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
-                    _In_ PWSTR lpCmdLine, _In_ int nCmdShow)
+int WINAPI wWinMain(_In_ HINSTANCE inst, _In_opt_ HINSTANCE prevInst,
+                    _In_ PWSTR cmdLine, _In_ int cmdShow)
 {
-   WNDCLASSEXW wc;
-   HWND        hwnd;
-   MSG         Msg;
+   UNREFERENCED_PARAMETER(prevInst);
+   UNREFERENCED_PARAMETER(cmdLine);
+
+   WNDCLASSEXW wc        = { 0 };
+   HWND        wnd;
+   MSG         msg;
+   PCWSTR      className = L"myWindowClass";
 
    wc.cbSize        = sizeof(WNDCLASSEXW);
    wc.style         = CS_VREDRAW | CS_HREDRAW;
    wc.lpfnWndProc   = WndProc;
    wc.cbClsExtra    = 0;
    wc.cbWndExtra    = 0;
-   wc.hInstance     = hInstance;
-   wc.hIcon         = (HICON) LoadImageW(NULL, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
-   wc.hIconSm       = (HICON) LoadImageW(NULL, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
+   wc.hInstance     = inst;
+   wc.hIcon         = (HICON)   LoadImageW(NULL, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
+   wc.hIconSm       = (HICON)   LoadImageW(NULL, IDI_APPLICATION, IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
    wc.hCursor       = (HCURSOR) LoadImageW(NULL, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED);
-   wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+   wc.hbrBackground = (HBRUSH)  (COLOR_WINDOW + 1);
    wc.lpszMenuName  = MAKEINTRESOURCEW(IDR_MENU);
-   wc.lpszClassName = g_szClassName;
+   wc.lpszClassName = className;
 
    if ( !RegisterClassExW(&wc) )
    {
@@ -213,26 +216,26 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
       return 0;
    }
 
-   hwnd = CreateWindowExW(WS_EX_CLIENTEDGE,
-                          g_szClassName,
-                          L"A Font Program",
-                          WS_OVERLAPPEDWINDOW,
-                          CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
-                          NULL, NULL, hInstance, NULL);
+   wnd = CreateWindowExW(WS_EX_CLIENTEDGE,
+                         className,
+                         L"A Font Program",
+                         WS_OVERLAPPEDWINDOW,
+                         CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+                         NULL, NULL, inst, NULL);
 
-   if ( hwnd == NULL )
+   if ( wnd == NULL )
    {
       MessageBoxW(NULL, L"Window Creation Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
       return 0;
    }
 
-   ShowWindow(hwnd, nCmdShow);
-   UpdateWindow(hwnd);
+   ShowWindow(wnd, cmdShow);
+   UpdateWindow(wnd);
 
-   while ( GetMessageW(&Msg, NULL, 0, 0) > 0 )
+   while ( GetMessageW(&msg, NULL, 0, 0) > 0 )
    {
-      TranslateMessage(&Msg);
-      DispatchMessageW(&Msg);
+      TranslateMessage(&msg);
+      DispatchMessageW(&msg);
    }
-   return (int) Msg.wParam;
+   return (int) msg.wParam;
 }
