a046e2f5f5c6416bdd5dfa2a4e618df1fc499c13
George Pimpleton <george_pimpleton@hotmail.com>
2025-12-08 09:34:43 -0800

    Added Menu One example

---
+++ revised-code/03_menu_one/menu_one_modern/resource.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define IDR_MYMENU               101
+#define IDI_MYICON               102
+#define ID_FILE_EXIT             40001
+#define ID_STUFF_GO              40002
+#define ID_STUFF_GOSOMEWHEREELSE 40003
